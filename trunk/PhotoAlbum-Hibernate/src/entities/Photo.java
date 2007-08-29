66526530dba8fb9c93178ed69785eb7376e6d492
marin.n.dimitrov <marin.n.dimitrov@bfab8106-4c38-0410-87d6-6bd745779a30>
2007-08-29 15:52:08 +0000

    Java classes

---
+++ trunk/PhotoAlbum-Hibernate/src/entities/Photo.java
@@ -0,0 +1,58 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Photo {
+
+	private int photoId;
+
+	private String name;
+
+	private String path;
+
+	private Category category;
+
+	private List<Comment> comments = new ArrayList<Comment>();
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public int getPhotoId() {
+		return photoId;
+	}
+
+	public void setPhotoId(int photoId) {
+		this.photoId = photoId;
+	}
+
+}
