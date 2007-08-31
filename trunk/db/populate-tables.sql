cda445337dc029e2b5365fd7310fb196e7caaebd
rosenspasov <rosenspasov@bfab8106-4c38-0410-87d6-6bd745779a30>
2007-08-31 22:09:43 +0000

    Added records for users and categories tables.

--- trunk/db/populate-tables.sql
+++ trunk/db/populate-tables.sql
@@ -1,5 +1,17 @@
 INSERT INTO USERS
-VALUES(1, 'volen', 'Volen', 'Dimitrov', 'volen')
+VALUES(1, 'volen', 'Volen', 'Dimitrov', 'dimitrov')
+/
+
+INSERT INTO USERS
+VALUES(2, 'rosen', 'Rosen', 'Spasov', 'spasov')
+/
+
+INSERT INTO USERS
+VALUES(3, 'zahary', 'Zahary', 'Anastasov', 'anastasov')
+/
+
+INSERT INTO USERS
+VALUES(4, 'marin', 'Marin', 'Dimitrov', 'dimitrov')
 /
 
 COMMIT
